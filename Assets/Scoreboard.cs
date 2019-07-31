cc921296f5aa5b54ffc5f3fdce50474e1cf5f873
SiebenHans <274962828@qq.com>
2019-07-31 20:55:46 +0800

    [Change] 采用模板形式

--- Assets/Scoreboard.cs
+++ Assets/Scoreboard.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Scoreboard : MonoBehaviour
 {
-    public ScoreboardItem templatePrafab;   // 实例化的模板预制
+    public GameObject TemplateItem;     // 实例化的模板
     public int Count = 1;       // 显示几位数字
 
     // 需要展示的数字，之后将会按“位”分配到每个Item上
@@ -20,15 +20,33 @@ public class Scoreboard : MonoBehaviour
         _maxNumber = 1;
         for (int i = 0; i < Count; i++)
         {
-            ScoreboardItem itemObj = Instantiate(templatePrafab);
+            GameObject itemObj = Instantiate(TemplateItem);
+            ScoreboardItem scoreboardItem = itemObj.GetComponent<ScoreboardItem>();
             itemObj.transform.SetParent(transform, false);
-            _itemList.Add(itemObj);
+            _itemList.Add(scoreboardItem);
             _numberList.Add(0);
             _maxNumber *= 10;
         }
         _maxNumber -= 1;
+        TemplateItem.gameObject.SetActive(false);
     }
 
+    /// <summary>
+    /// 设置最大数字
+    /// </summary>
+    public void SetMaxNumber(int maxNumber)
+    {
+        if (maxNumber > _maxNumber)
+        {
+            Debug.LogError("Cannot set max number which is over the default one. _maxNumber: " + _maxNumber);
+            return;
+        }
+        _maxNumber = maxNumber;
+    }
+
+    /// <summary>
+    /// 设置目标数字
+    /// </summary>
     public void SetNumber(int number)
     {
         number = Mathf.Clamp(number, 0, _maxNumber);
